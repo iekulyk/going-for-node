9a95212498ce21b40b9de3c08a9b78fd51db25af
Evgeniy Kulyk <kulyk.evgeniy@gmail.com>
2015-11-13 02:17:25 +0200

    adding socket.io

---
+++ updater/index.js
@@ -0,0 +1,13 @@
+(function (updater) {
+
+	var socketio = require("socket.io");
+
+	updater.init = function (server) {
+		var io = socketio.listen(server);
+
+		io.sockets.on("connection", function (socket) {
+			console.log("socket was connected");
+		});
+	}
+
+})(module.exports);
